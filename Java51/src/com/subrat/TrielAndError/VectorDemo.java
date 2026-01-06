05c4b029decccd2b144bf1d3dad6d39e5f6ce8e5
subratnaik1 <subratnaik2008@gmail.com>
2026-01-06 22:11:59 +0530

    Triel

---
+++ Java51/src/com/subrat/TrielAndError/VectorDemo.java
@@ -0,0 +1,46 @@
+package com.subrat.TrielAndError;
+
+
+public class VectorDemo
+{
+	public static void main(String[] args) 
+	{
+		Moof one = new Moof(8);
+		Moof two = new Moof(8);
+		if(one.equals(two))
+		{
+		System.out.println("Equal ...");
+		}
+		else
+		{
+		System.out.println("Not Equal ...");
+		}
+	}
+}
+class Moof
+{
+	private int moofValue;
+	Moof(int val)
+	{
+		moofValue=val;
+	}
+
+	public int getMoofValue()
+	{
+		return moofValue;
+	}
+	
+
+	
+public boolean equals(Object o)
+{
+	if((o instanceof Moof)&&(((Moof)o).getMoofValue()==this.moofValue))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	}
+}
