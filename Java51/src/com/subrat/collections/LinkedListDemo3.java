1c6a237f58353c817c4abc3f9bfa679e44cb076e
subratnaik1 <subratnaik2008@gmail.com>
2026-01-17 16:31:08 +0530

    new

---
+++ Java51/src/com/subrat/collections/LinkedListDemo3.java
@@ -0,0 +1,27 @@
+package com.subrat.collections;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class LinkedListDemo3 {
+	void main() {
+		LinkedList<String> city = new LinkedList<>();
+		city.add("Kolkata");
+		city.add("Banglore");
+		city.add("Hydrabad");
+		city.add("pune");
+		IO.println(city);
+		ListIterator<String> it = city.listIterator();
+		while (it.hasNext()) {
+			String cityName = it.next();
+			if (cityName.equals("Kolkata")) {
+				it.remove();
+			} else if (cityName.equals("Hydrabad")) {
+				it.add("ameerpeth");
+			} else if (cityName.equals("pune")) {
+				it.set("Mumbai");
+			}
+		}
+		city.forEach(System.out::println);
+	}
+}
