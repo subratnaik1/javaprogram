d5b336c74a2eaea9b4630ccfeaa6fad3c537c5fe
subratnaik1 <subratnaik2008@gmail.com>
2026-02-09 21:09:14 +0530

    new one

---
+++ Java51/src/com/subrat/StreamAPI/A6.java
@@ -0,0 +1,15 @@
+package com.subrat.StreamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A6 {
+void main() {
+	List <String>l1=new ArrayList<>();
+	l1.add("subrat");
+	l1.add("shubham");
+	l1.add("azad");
+	l1.add("bharat");
+	l1.stream()
+}
+}
