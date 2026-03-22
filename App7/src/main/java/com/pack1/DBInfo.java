ce8b7d66b406f78d412373df4d5f597b9fe2032c
subrat naik <subratnaik2008@gmail.com>
2026-03-22 22:31:14 +0530

    .....

--- App7/src/main/java/com/pack1/DBInfo.java
+++
@@ -1,8 +0,0 @@
-package com.pack1;
-
-public interface DBInfo {
-	String driver = "oracle.jdbc.OracleDriver";
-	String dbUrl = "jdbc:oracle:thin:@localhost:1521:orcl";
-	String dbUname = "subrat";
-	String dbPwd = "67890";
-}
--- App9/src/main/java/pack1/DBinfo.java
+++
@@ -1,8 +0,0 @@
-package pack1;
-
-public interface DBinfo {
-	String driver = "oracle.jdbc.OracleDriver";
-	String dbUrl = "jdbc:oracle:thin:@localhost:1521:orcl";
-	String dbUname = "subrat";
-	String dbPwd = "67890";
-}
