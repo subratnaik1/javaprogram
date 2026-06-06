4cdfe57907c4f1a1eec33aa1efe8a2b5241aecd0
subrat naik <subratnaik2008@gmail.com>
2026-06-06 23:37:13 +0530

    ..

--- StreamAPI/src/com/subrat/advancedLogicalQu/Practice.java
+++ StreamAPI/src/com/subrat/advancedLogicalQu/Practice.java
@@ -5,17 +5,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.OptionalInt;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-import javax.xml.stream.events.Characters;
 
 public class Practice {
-public static void main(String[] args) {
-	String [] s = {"abc", "123", "456", "xyz"};
-	List<Integer> list = Arrays.stream(s).filter(c->c.chars().allMatch(ch->Character.isDigit(ch))).map(m->Integer.parseInt(m)).toList();
-System.out.println(list);
-}
+	public static void main(String[] args) {
+		String s[]={"subrat","rahul"};
+		Object[] a=Arrays.stream(s).map(c->Comparator.reverseOrder()).toArray();
+		System.out.println(Arrays.toString(a));
+	}
 }
